7eff5771f935e26f084467155b9eb52060b06228
Florian Schmid <melonencool@gmail.com>
2022-01-16 21:50:05 +0100

    first push

---
+++ Components.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleClock
+{
+    class Components
+    {
+        private static List<int> ALL_EMPTY = new List<int> { 0, 0, 0, 0, 0 };
+        private static List<int> ALL_FILLED = new List<int> { 1, 1, 1, 1, 1 };
+        private static List<int> DOUBLE_DOT = new List<int> { 1, 0, 0, 0, 1 };
+        private static List<int> DOT_RIGHT = new List<int> { 0, 0, 0, 0, 1 };
+        private static List<int> DOT_LEFT = new List<int> { 1, 0, 0, 0, 0 };
+        private static List<int> DOT_MIDDLE = new List<int> { 0, 0, 1, 0, 0 };
+
+        public static List<List<int>> DIGIT_ZERO = new List<List<int>> { ALL_FILLED, DOUBLE_DOT, DOUBLE_DOT, DOUBLE_DOT, ALL_FILLED };
+        public static List<List<int>> DIGIT_ONE = new List<List<int>> { DOT_RIGHT, DOT_RIGHT, DOT_RIGHT, DOT_RIGHT, DOT_RIGHT };
+        public static List<List<int>> DIGIT_TWO = new List<List<int>> { ALL_FILLED, DOT_RIGHT, ALL_FILLED, DOT_LEFT, ALL_FILLED };
+        public static List<List<int>> DIGIT_THREE = new List<List<int>> { ALL_FILLED, DOT_RIGHT, ALL_FILLED, DOT_RIGHT, ALL_FILLED };
+        public static List<List<int>> DIGIT_FOUR = new List<List<int>> { DOUBLE_DOT, DOUBLE_DOT, ALL_FILLED, DOT_RIGHT, DOT_RIGHT };
+        public static List<List<int>> DIGIT_FIVE = new List<List<int>> { ALL_FILLED, DOT_LEFT, ALL_FILLED, DOT_RIGHT, ALL_FILLED };
+        public static List<List<int>> DIGIT_SIX = new List<List<int>> { ALL_FILLED, DOT_LEFT, ALL_FILLED, DOUBLE_DOT, ALL_FILLED };
+        public static List<List<int>> DIGIT_SEVEN = new List<List<int>> { ALL_FILLED, DOT_RIGHT, DOT_RIGHT, DOT_RIGHT, DOT_RIGHT };
+        public static List<List<int>> DIGIT_EIGHT = new List<List<int>> { ALL_FILLED, DOUBLE_DOT, ALL_FILLED, DOUBLE_DOT, ALL_FILLED};
+        public static List<List<int>> DIGIT_NINE = new List<List<int>> { ALL_FILLED, DOUBLE_DOT, ALL_FILLED, DOT_RIGHT, ALL_FILLED };
+
+        public static List<List<int>> SEMI_COLUMN = new List<List<int>> {ALL_EMPTY, DOT_MIDDLE, ALL_EMPTY, DOT_MIDDLE, ALL_EMPTY };
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleClock
+{
+    class Program
+    {
+        private static int clock_size = 7;
+
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.Clear();
+
+
+                //Example 1:
+                draw(convert_time(DateTime.Now.ToString("HH:mm")));
+
+                //Example 2:
+                // draw(convert_time(DateTime.Now.ToString("HH:mm:ss")));
+
+                System.Threading.Thread.Sleep(1000);
+            }
+
+            Console.ReadLine();
+        }
+
+        private static void draw(List<List<List<int>>> data_array)
+        {
+            int counter = 0;
+            foreach (List<int> line in data_array[0]) // For line
+            {
+                for (int y = 0; y < clock_size; y++) // von oben nach unten die Breite
+                {
+                    for (int i = 0; i < data_array.Count; i++)
+                    {
+                        foreach (int data_byte in data_array[i][counter])
+                        {
+                            string char_to_print = ""; // can be a space or a # sign
+                            if (data_byte == 0)
+                            {
+                                char_to_print = " ";
+                            }
+                            else if (data_byte == 1)
+                            {
+                                char_to_print = "#";
+                            }
+                            else
+                            {
+                                Console.WriteLine("xxxxxxxxxx\nBROKEN DATA or DATA MISSING\nxxxxxxxxxx");
+                                return;
+                            }
+
+                            for (int k = 0; k < clock_size; k++)
+                            {
+                                Console.Write(char_to_print);
+                            }
+                        }
+                        Console.Write("    "); // Space between digits
+                    }
+                    Console.WriteLine("");
+                }
+                counter += 1;
+            }
+        }
+
+        private static List<List<List<int>>> convert_time(string time)
+        {
+            List<List<List<int>>> data_in_right_format = new List<List<List<int>>>();
+
+            foreach (char c in time)
+            {
+                switch (c)
+                {
+                    case '0':
+                        data_in_right_format.Add(Components.DIGIT_ZERO);
+                        break;
+                    case '1':
+                        data_in_right_format.Add(Components.DIGIT_ONE);
+                        break;
+                    case '2':
+                        data_in_right_format.Add(Components.DIGIT_TWO);
+                        break;
+                    case '3':
+                        data_in_right_format.Add(Components.DIGIT_THREE);
+                        break;
+                    case '4':
+                        data_in_right_format.Add(Components.DIGIT_FOUR);
+                        break;
+                    case '5':
+                        data_in_right_format.Add(Components.DIGIT_FIVE);
+                        break;
+                    case '6':
+                        data_in_right_format.Add(Components.DIGIT_SIX);
+                        break;
+                    case '7':
+                        data_in_right_format.Add(Components.DIGIT_SEVEN);
+                        break;
+                    case '8':
+                        data_in_right_format.Add(Components.DIGIT_EIGHT);
+                        break;
+                    case '9':
+                        data_in_right_format.Add(Components.DIGIT_NINE);
+                        break;
+                    case ':':
+                        data_in_right_format.Add(Components.SEMI_COLUMN);
+                        break;
+                    default:
+                        Console.WriteLine("Something Went Wrong!");
+                        return data_in_right_format;
+                }
+            }
+            return data_in_right_format;
+        }
+    }
+}
